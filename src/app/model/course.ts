c1bdee0869862ec6752d802667c91d99cacbcc32
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-05-09 18:31:31 +0200

    Skapat interface och service för http-request

--- src/app/model/course.ts
+++ src/app/model/course.ts
@@ -1,2 +1,12 @@
 export interface Course {
+    courseCode: string,
+    subjectCode: string,
+    level: string,
+    progression: string,
+    courseName: string,
+    points: number,
+    institutionCode: string,
+    subject: string,
+    syllabus: string
 }
+
