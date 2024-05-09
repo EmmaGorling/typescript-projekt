0075a497669226200e339f8b5fa3e985210f723a
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-05-09 10:17:37 +0200

    Har kommit en bit på uppstarten av projektet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { MylistComponent } from './mylist/mylist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'courses', component: CoursesComponent},
+    {path: 'mylist', component: MylistComponent}
+];
