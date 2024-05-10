acc84a9e1aac9c06ef66a3a4c7d6d9ca39a374e1
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-05-10 09:24:29 +0200

    Skapat instans av courseService i course-component och gjrot lyckat http-anrop

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CourseService } from '../services/course.service';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
-  
+  //Arrays for storing the data
+  courseList: Course[] = [];
+  orgCourseList: Course[] = [];
+
+  // Create a instance of courseservice
+  constructor(private courseservice: CourseService) {}
+
+  ngOnInit() {
+    this.courseservice.getCourses().subscribe(data => {
+      this.courseList = data;
+      this.orgCourseList = data;
+    })
+  }
 }
+
